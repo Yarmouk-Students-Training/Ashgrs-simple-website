1809287db3829c4af7d654005b1ca3d28e317ba1
Ashgr <abodyashgr@gmail.com>
2021-03-16 00:50:57 +0200

    done with lodash

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const http = require('http');
+const _ = require('lodash');
 function getUrl(page,newPage){
+    // this function return the correct url the user want.
     if(newPage == '/' || newPage =='/index'){
         page+=('/index.html');
     }
-    else if(newPage == '/about'){
+    else if(newPage == '/about-us'){
         page+=('/about.html');
     }
     else{
@@ -13,7 +15,12 @@ function getUrl(page,newPage){
     return page;
 }
 const server = http.createServer((req, res) =>{
-    console.log("am just requesting",req.url);
+    // console.log("am just requesting",req.url);
+    
+    // lodash here
+    var num = _.random(1,5);
+    console.log(num);
+    // returning html pages
     res.setHeader('content-type' , 'text/html');
     var path = getUrl('./views' , req.url)
     if(path == './views/404.html'){
