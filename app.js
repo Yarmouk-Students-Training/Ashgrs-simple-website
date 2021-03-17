ca7b824abc2f687aeac15005c13b39b8d403d283
Ashgr <abodyashgr@gmail.com>
2021-03-17 20:00:43 +0200

    done with all

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const db_uri = 'mongodb+srv://ashgr:ashgr123@node-tutorial.0wsmi.mongodb.net/nod
 
 mongoose.connect(db_uri , {useNewUrlParser:true , useUnifiedTopology:true})
     .then((result)=>{
-        console.log("Database connected successfully");
+        console.log("=== Database connected successfully ===");
         app.listen(3000);
     })
     .catch((err)=>console.log(err));
@@ -20,14 +20,54 @@ mongoose.connect(db_uri , {useNewUrlParser:true , useUnifiedTopology:true})
 // setup view enging
 app.set('view engine' , 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded());
 app.use(morgan('dev'));
 
 
 // express app pages
+
 app.get('/' , (req, res) =>{
     res.redirect('/blogs');
 });
 
+
+
+app.post('/blogs' , (req,res)=>{
+    // console.log(req.body);
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result)=>{
+            res.redirect('/blogs');
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
+app.get('/blogs/create' , (req, res) =>{
+    res.render('create',{title : 'New Blog'});
+});
+app.get('/blogs/:id' , (req,res)=>{
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result)=>{
+            res.render('details',{blog:result , title : "Blog Detail"});
+        })
+        .catch((err) =>{
+            console.log(err);
+        });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+      .then(result => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 
 app.get('/about' , (req, res) =>{
     res.render('about',{title : 'About'});
@@ -43,9 +83,7 @@ app.get('/blogs' , (req,res) =>{
         });
 });
 
-app.get('/blogs/create' , (req, res) =>{
-    res.render('create',{title : 'New Blog'});
-});
+
 
 app.use((req,res) =>{
     res.status(404).render('404',{title : '404'});
