0d493056ff3acaf913f2f4258c090cf5b3899d35
Ashgr <abodyashgr@gmail.com>
2021-03-18 01:21:06 +0200

    done with website

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const { result } = require('lodash');
-const Blog = require('./models/blogs');
+const blogRoutes = require('./routes/blogRoutes');
 
 // creating app and connecting database
 //81.253.108.82
@@ -30,60 +29,11 @@ app.get('/' , (req, res) =>{
     res.redirect('/blogs');
 });
 
-
-
-app.post('/blogs' , (req,res)=>{
-    // console.log(req.body);
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result)=>{
-            res.redirect('/blogs');
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
-});
-app.get('/blogs/create' , (req, res) =>{
-    res.render('create',{title : 'New Blog'});
-});
-app.get('/blogs/:id' , (req,res)=>{
-    const id = req.params.id;
-    Blog.findById(id)
-        .then((result)=>{
-            res.render('details',{blog:result , title : "Blog Detail"});
-        })
-        .catch((err) =>{
-            console.log(err);
-        });
-});
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Blog.findByIdAndDelete(id)
-      .then(result => {
-        res.json({ redirect: '/blogs' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-
 app.get('/about' , (req, res) =>{
     res.render('about',{title : 'About'});
 });
 
-app.get('/blogs' , (req,res) =>{
-    Blog.find()
-        .then(result=>{
-            res.render('index' , {title:"All Blogs" , blogs:result})
-        })
-        .catch(err =>{
-            console.log(err);
-        });
-});
-
-
+app.use('/blogs',blogRoutes);
 
 app.use((req,res) =>{
     res.status(404).render('404',{title : '404'});
