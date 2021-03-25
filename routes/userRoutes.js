3991c9be59d82fa6a61a68874fe8c54cefeac608
Abdalrahman Al-Ashgr <53108136+Ashgr@users.noreply.github.com>
2021-03-25 13:47:10 +0200

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express') ;
-const userController = require('../controllers/userController');
-const router = express.Router();
-
-router.get('/register' , userController.register_get);
-router.get('/registerError' , userController.register_get_err);
-router.post('/register' , userController.register_post);
-
-module.exports = router;
