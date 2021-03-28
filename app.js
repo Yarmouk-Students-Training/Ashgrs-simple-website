3e46d62c0c16849d58e492d15b08b64b2f736e7e
Abdalrahman Al-Ashgr <53108136+Ashgr@users.noreply.github.com>
2021-03-28 15:51:55 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const jwt = require('jsonwebtoken')
 const { sequelize} = require('./models')
 const models = require('./models')
 const postRoutes = require('./router/postRoutes.js');
@@ -9,6 +10,55 @@ const friendRouter = require('./router/friendRouter.js')
 
 const app = express()
 app.use(express.json())
+
+app.get('/api' , (req,res)=>{
+  return res.json({
+    messege : "we are in api route (:"
+  })
+})
+
+app.post('/api/login', (req, res) => {
+  const user = {
+    id: 1, 
+    username: 'fake',
+    email: 'fake@gmail.com'
+  }
+
+  jwt.sign({user}, 'secretkey', (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {  
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } 
+    else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
+});
+
+function verifyToken(req, res, next) { 
+  const tempHeader = req.headers['authorization'];
+  if(typeof tempHeader !== 'undefined') {
+    const temp = tempHeader.split(' ');
+    console.log(temp);
+    console.log("==============")
+    const tempToken = temp[1];
+    req.token = tempToken;
+    next();
+  } 
+  else {
+    res.sendStatus(403);
+  }
+}
 
 app.use('/users', userRoutes);
 app.use('/posts',postRoutes);
