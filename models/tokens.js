51b26b508bc07d95612a7a3e5d0376449ba9372f
Abdalrahman Al-Ashgr <53108136+Ashgr@users.noreply.github.com>
2021-03-29 19:52:27 +0300

    Add files via upload

--- models/tokens.js
+++ models/tokens.js
@@ -9,8 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({user}) {
       // define association here
+      this.hasOne(user , {foreignKey:"userEmail"});
     }
   };
   tokens.init({
--- models/user.js
+++ models/user.js
@@ -9,11 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({post , comment,friendship}) {
+    static associate({post , comment,friendship ,tokens}) {
       // define association here
       this.hasMany(post , {foreignKey:"userEmail"});
       this.hasMany(comment , {foreignKey:"userEmail"});
       this.belongsToMany(this,{through:friendship,as:'sender' ,foreignKey:'reciver'})
+      this.belongsTo(tokens , {foreignKey:"userEmail"})
     } 
   };
   user.init({
