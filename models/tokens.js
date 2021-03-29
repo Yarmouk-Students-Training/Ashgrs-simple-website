6ef0133798470caa97580dcebc8611e4d3e9552d
Abdalrahman Al-Ashgr <53108136+Ashgr@users.noreply.github.com>
2021-03-29 19:55:39 +0300

    Update tokens.js

--- models/tokens.js
+++ models/tokens.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate({user}) {
       // define association here
-      this.hasOne(user , {foreignKey:"userEmail"});
+      this.belongsTo(user , {foreignKey:"userEmail"});
     }
   };
   tokens.init({
